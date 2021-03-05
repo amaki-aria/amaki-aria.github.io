b61e2f12ad8134f8a45418918acbb1c9ad6bcf94
abc <abc@example.com>
2021-03-06 02:44:49 +0800

    feature: 一体化的更新工具

--- build/buildIndex.js
+++ build/buildIndex.js
@@ -2,17 +2,16 @@ const fs = require('fs')
 const utility = require('./utility')
 const path = require('path');
 
-const res = {};
-
 function build(p) {
   if (path.extname(p) == '.md') {
     const base = path.basename(p, '.md')
     if (base.includes("index")) return;
-    const title = utility.readMd(p).data.title;
+    const data = utility.readMd(p).data;
     // console.log(base, title);
     return {
       base,
-      title,
+      title: data.title,
+      weight: data.weight,
     }
   } else if (utility.isDir(p)){
     let childs = fs.readdirSync(p);
@@ -22,7 +21,9 @@ function build(p) {
       childs: []
     };
     if (fs.existsSync(path.join(p, '_index.md'))) {
-      res.title = utility.readMd(path.join(p, '_index.md')).data.title;
+      const data = utility.readMd(path.join(p, '_index.md')).data;
+      res.title = data.title;
+      res.weight = data.weight;
     }
     if (!res.title) res.title = res.base;
 
